111349cb2e161ce85a72bac4f831772e7efa18e8
Nikhil Soni <124785555+Nikhilsoni-2003@users.noreply.github.com>
2024-04-15 04:05:23 -0700

    ques2

---
+++ OopsLab3/src/MyPackage/program2.java
@@ -0,0 +1,18 @@
+package MyPackage;
+
+class dog{
+	public String name;
+	public String breed;
+	
+	public dog(String name, String breed) {
+		this.name=name;
+		this.breed=breed;
+	}
+}
+public class program2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
