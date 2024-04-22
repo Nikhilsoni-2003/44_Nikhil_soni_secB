6619091ee2b0b00a16f2d0785c09ced70f22a248
Nikhil Soni <124785555+Nikhilsoni-2003@users.noreply.github.com>
2024-04-22 15:35:57 +0530

    Update Program4.java

--- OopsLab2/Program4.java
+++ OopsLab2/Program4.java
@@ -1,5 +1,33 @@
 package OopsLab2;
 
-public class Program4 {
+import java.util.Scanner;
 
+public class Program4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        scanner.close();
+
+        if (isPalindrome(input)) {
+            System.out.println("The input string is a palindrome.");
+        } else {
+            System.out.println("The input string is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
 }
+
