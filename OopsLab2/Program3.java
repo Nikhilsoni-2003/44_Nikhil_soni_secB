2175916696c4d9c49c3f071d00b4074442fa338d
Nikhil Soni <124785555+Nikhilsoni-2003@users.noreply.github.com>
2024-04-22 15:35:29 +0530

    Update Program3.java

--- OopsLab2/Program3.java
+++ OopsLab2/Program3.java
@@ -1,5 +1,35 @@
 package OopsLab2;
 
-public class Program3 {
+import java.util.Scanner;
 
+public class Program3 {
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3, 4}, {1, 2}, {1, 2, 3, 4, 5}};
+        System.out.println("Original data in the array:");
+        printArray(array);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter new data to replace the existing array elements:");
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print("Enter element at position [" + i + "][" + j + "]: ");
+                array[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Data in the array after scanning:");
+        printArray(array);
+
+        scanner.close();
+    }
+
+    public static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
